39cdb82162bbe2fd293dc85d5b7648648567f879
gil-unx <57790408+gil-unx@users.noreply.github.com>
2021-05-20 23:33:23 -0700

    Create FILE.cs

---
+++ KPD/KPDUNPACKER/FILE.cs
@@ -0,0 +1,16 @@
+namespace KPDUNPACKER
+{
+
+    class FILE
+	{
+		public int infosOffset { get; set; }
+		public int index{get;set;}
+		public long refOffset { get; set; }
+		public string name{get;set;}
+		public long offset{get;set;}
+		public long size{get;set;}
+	}
+
+
+
+}
